b062bede201012c3b634d51847acf176c6a6e957
emmanuel <mannypaz15@gmail.com>
2023-07-02 08:28:08 -0500

    hiring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import logo from './oasis.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='announcement-container'>
+        <div className='logo-wrapper'>
+          <img className='logo' alt='logo' src={logo}></img>
+        </div>
+        <div className='coming-soon-wrapper'>
+          <p className='coming-soon-text'>Coming soon!</p>
+        </div>
+        <div className='apply-text-wrapper'>
+          <p className='apply-text'>Now hiring! Please apply with the following <a href="https://i.kym-cdn.com/photos/images/newsfeed/001/709/919/ce7.png" target="_blank" rel="noreferrer">link!</a></p>
+        </div>
+      </div>
     </div>
   );
 }
