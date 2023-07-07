f3ad1ba071d8a059471c2777e9c03c4558f169b5
epaz <epaz@iastate.edu>
2023-07-07 14:31:18 -0500

    link

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           <p className='coming-soon-text'>Coming soon!</p>
         </div>
         <div className='apply-text-wrapper'>
-          <p className='apply-text'>Now hiring! Please apply with the following <a href="https://i.kym-cdn.com/photos/images/newsfeed/001/709/919/ce7.png" target="_blank" rel="noreferrer">link!</a></p>
+          <p className='apply-text'>Now hiring! Please apply with the following <a href="https://forms.gle/LN1YEdix5HdPaqJR7" target="_blank" rel="noreferrer">link!</a></p>
         </div>
       </div>
     </div>
