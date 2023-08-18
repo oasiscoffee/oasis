be1e18ba57a243f01bbc5dc0bc01b6bdffa86c89
emmpaz <mannypaz15@gmail.com>
2023-08-18 17:04:39 -0500

    edit

--- src/core-pages/AboutUs.js
+++ src/core-pages/AboutUs.js
@@ -34,18 +34,22 @@ const AboutUs = () => {
                     </div>
                     <div className="about-text-paragraph-container">
                         <p>
-                           page coming soon.
+                        Welcome to Oasis Coffee Shop, brought to life by siblings Rubi, Lesly, and Emmanuel. As a tight-knit family, we're thrilled to invite you into our coffee haven, nestled right here in our hometown of Winfield. With shared laughter, dreams, and a passion for crafting delightful experiences, we're joyfully serving up more than just coffee and delicious food items - we're sharing a piece of our hearts with every sip and bite.<br></br><br></br>
+
+Growing up together in Winfield, we've seen the power of community firsthand, and it's with immense excitement that we step into this role of serving you. Our coffee shop isn't just a place to grab a quick drink; it's an oasis where connections are nurtured, conversations flow, and smiles are exchanged.<br></br><br></br>
+
+The credit for our journey goes to our parents, who instilled in us the values of hard work, dedication, and the importance of fostering relationships. Their guidance has shaped Oasis Coffee Shop into what it is today – a warm, welcoming space that pays homage to our roots while embracing the future. So whether you're seeking a serene spot to unwind or a vibrant space to meet with friends, we invite you to join us at Oasis Coffee Shop. Together, we'll continue the tradition of community, family, and a shared love for the simple joys of life – all in the heart of Winfield.<br></br><br></br>
                         </p>
                     </div>
-                    {/* <div className="about-text-subtext-container">
+                    <div className="about-text-subtext-container">
                         <p>
                             Big thanks to our help!
                         </p>
-                    </div> */}
+                    </div>
                 </div>
-                {/* <div className="about-big-image-container">
+                <div className="about-big-image-container">
                     <div className="img-placeholder"></div>
-                </div> */}
+                </div>
             </div>
             <MenuPages setting={open}/>
         </div>
